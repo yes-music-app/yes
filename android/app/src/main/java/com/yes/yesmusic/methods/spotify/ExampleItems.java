0542a005b3cd42c88262617704d83ca1f127579b
JMichaelYang <jaewonmichael@gmail.com>
2019-02-21 21:29:19 -0800

    Added example items

---
+++ android/app/src/main/java/com/yes/yesmusic/methods/spotify/ExampleItems.java
@@ -0,0 +1,6 @@
+package com.yes.yesmusic.methods.spotify;
+
+class ExampleItems {
+    static final String REDIRECT_URI = "...";
+    static final String CLIENT_ID = "...";
+}
--- lib/blocs/phone_number_bloc.dart
+++
@@ -1,8 +0,0 @@
-import 'package:yes_music/blocs/bloc_provider.dart';
-
-class PhoneNumberBloc implements BlocBase {
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
-}
